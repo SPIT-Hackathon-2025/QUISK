cd5c700af6f01bcf2566e7c2127b0f0ae8bdedc3
HARSH825 <harshchhallani3@gmail.com>
2025-02-08 18:13:01 +0530

    add /properties route

--- backend/route/leaseRoute.js
+++ backend/route/leaseRoute.js
@@ -5,11 +5,12 @@ const router = Router();
 import leaserSignup from "../controller/leaseSignup";
 import leaserLogin  from "../controller/leaseLogin";
 import listProperty from "../controller/listProperty";
-
+import ownedProperties from "../controller/ownedProperties";
 // Lease Routes
 router.post("/signup", leaserSignup);
 router.post("/login", leaserLogin);
 router.post("/list",listProperty);
+router.get("/properties/:_id",ownedProperties);
 
 
 export default router;
