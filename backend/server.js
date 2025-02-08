f66e450cdf7bf6d52b2f81defc9da7b71dbb7618
HARSH825 <harshchhallani3@gmail.com>
2025-02-08 16:18:17 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors'; 
+import dotenv from 'dotenv'
+import connectDb from './db/connectDb.js';
+dotenv.config();
+const app = express(); 
+app.use(express.json());
+// import leaseRouter from './routes/leaseRouter.js';
+// import rentRouter from './routes/rentRouter.js';
+// app.use('/lease',leaseRouter);
+// app.use('/rent',rentRouter);
+ app.use(cors()); 
+ 
+ connectDb();  
+ 
+const PORT = process.env.PORT;
+
+app.listen(PORT, ()=>{
+    console.log("Server connected to : "+PORT);
+})
